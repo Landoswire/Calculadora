bf2db3815425071ae006a7797fc9442c52d514d4
Landoswire <61020744+Landoswire@users.noreply.github.com>
2020-09-21 00:12:09 -0500

    Create Cola.java

---
+++ Cola.java
@@ -0,0 +1,48 @@
+/**
+ *
+ * @author issei
+ */
+public class Cola {
+
+    NodoCola inicio = null;
+    NodoCola fin = null;
+
+    void queue(char dato) {
+        NodoCola nuevo = new NodoCola();
+        nuevo.dato = dato;
+        nuevo.siguiente = null;
+        nuevo.anterior = null;
+        if (inicio != null) {
+            inicio.anterior = nuevo;
+            nuevo.siguiente = inicio;
+            
+        } else {
+            
+            inicio = nuevo;
+            fin = nuevo;
+        }
+    }
+
+    char dequeue() {
+        char letra;
+        if (fin != null) {
+            NodoCola aux = fin;
+
+            if (inicio == fin) {
+                inicio = null;
+                //  System.out.println("un elemento");
+                fin = null;
+            } else {
+                fin = fin.anterior;
+                fin.siguiente = null;
+            }
+            aux.siguiente = null;
+            aux.anterior = null;
+            letra = aux.dato;
+        } else {
+            letra = 0;
+        }
+        return letra;
+    }
+
+}
